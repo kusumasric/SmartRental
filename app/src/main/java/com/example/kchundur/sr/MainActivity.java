7b27fdb0cbc01b31521525178bb079ce178c7d37
Chunduru <kusuma.sri.chunduru@intel.com>
2017-12-03 06:55:32 -0800

    adding first screen

---
+++ app/src/main/java/com/example/kchundur/sr/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.kchundur.sr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    private EditText et_Name,et_Pass;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et_Name = (EditText)findViewById(R.id.name);
+        et_Pass = (EditText)findViewById(R.id.password);
+    }
+
+    public void onclickLogin(View view)
+    {
+
+
+
+    }
+    public void onclickSignUp(View view)
+    {
+
+
+    }
+
+    private void LoginErrorMessage()
+    {
+        android.app.AlertDialog.Builder dlgAlert = new android.app.AlertDialog.Builder(this);
+        dlgAlert.setMessage("wrong password or username");
+        dlgAlert.setTitle("Error Message...");
+        dlgAlert.setPositiveButton("OK", null);
+        dlgAlert.setCancelable(true);
+        dlgAlert.show();
+    }
+
+}
